1c9e4b6cd18f1df83242aaceffb4516363c50822
bimodwilaksono <bimodwilaksono@gmail.com>
2022-11-21 01:53:36 +0700

    set up backend & create endpoint for current weather

---
+++ server.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import express from "express";
+import path from "path";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
+const app = express();
+// console.log(process.env.VITE_BASE_URL)
+
+// app.use('/', express.static(path.join(__dirname, "public")))
+
+app.get("/health", (req, res) => {
+    res.json({message: "Health is OK"});
+});
+
+app.get(`/current/:id`, async (req, res) => {
+    const { id } = req.params;
+    await axios
+        .get(`${process.env.VITE_BASE_URL}/currentconditions/v1/${id}?apikey=${process.env.VITE_API_KEY}`)
+        .then((resp) => res.json({ data: resp.data }))
+        .catch((err) => res.json({message: err.response.data.Message}));
+});
+
+app.listen(5000, () => {
+    console.log("App running on port 3000");
+});
