2d5030b610a0e3642cab4f663c7908c1c66008ba
bimodwilaksono <bimodwilaksono@gmail.com>
2022-12-11 11:20:41 +0700

    refactor code from submit event listener into onSubmit props

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
-import * as dayjs from "dayjs";
+import React from "react";
 
 const Search = (props) => {
     const { getCity } = props;
 
-    useEffect(() => {
-        const inputForm = document.querySelector("form");
-
-        inputForm.addEventListener("submit", (e) => {
-            // console.log(inputForm.city.value)
-            e.preventDefault();
-            const citySearch = inputForm.city.value.trim();
-            getCity(citySearch);
-        });
-
-        return () => {
-            inputForm.removeEventListener("submit", (e) => {
-                e.preventDefault();
-                const citySearch = inputForm.city.value.trim();
-                getCity(citySearch);
-            });
-        };
-    }, []);
-
     return (
-        <div className="mt-5 text-center text-white">
-            <p className='text-7xl font-semibold'>Weather App</p>
-            <form className='text-center mt-5'>
+        <div className='mt-5 text-center'>
+            <p className='text-7xl font-semibold text-white'>Weather App</p>
+            <form
+                className='text-center mt-5'
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    getCity(e.target[0].value)
+                }}>
                 <input
                     className='border-2 border-black rounded-md h-10 py-1 px-2 w-1/2'
                     type='text'
