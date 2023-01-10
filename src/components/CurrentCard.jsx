bed13305058304545b4ae231464c956cfeea13a1
bimodwilaksono <bimodwilaksono@gmail.com>
2023-01-10 21:10:37 +0700

    FE: Adjust Styling for Card

--- src/components/CurrentCard.jsx
+++ src/components/CurrentCard.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import dayjs from "dayjs";
 
+import { MdLocationPin } from "react-icons/md";
+
 const CurrentCard = (props) => {
     const { city, cityDetail } = props;
     const currentDate = dayjs(new Date()).format("ddd, D MMM");
     const cityName = city?.EnglishName;
     const countryName = city?.Country?.EnglishName;
     const currentTemp = cityDetail?.Temperature?.Metric?.Value;
-    const icon = cityDetail?.WeatherIcon
+    const icon = cityDetail?.WeatherIcon;
 
     return (
-        <div className='Card mx-auto text-center py-5 rounded-md w-48 h-48 bg-[#1b1c48] text-white'>
+        <div className='Card mx-auto text-center py-4 px-3 rounded-md w-60 h-60 bg-[#1b1c48] text-white'>
             <div className='flex justify-between'>
                 <p>Today</p>
                 <p>{currentDate}</p>
             </div>
-            <div className="flex">
-                <p>{`${currentTemp}°C`}</p>
-                <img src={icon.toString().length == 1 ? `https://developer.accuweather.com/sites/default/files/0${icon}-s.png` : `https://developer.accuweather.com/sites/default/files/${icon}-s.png`} />
+            <img
+                className='mx-auto mt-5 mb-3 h-20'
+                src={
+                    icon?.toString().length == 1
+                        ? `https://developer.accuweather.com/sites/default/files/0${icon}-s.png`
+                        : `https://developer.accuweather.com/sites/default/files/${icon}-s.png`
+                }
+            />
+            <p>{`${Math.floor(currentTemp)}°C`}</p>
+            <div className='flex gap-2 items-center mt-4'>
+                <MdLocationPin />
+                <p>
+                    {cityName}, {countryName}
+                </p>
             </div>
-            <p>
-                {cityName}, {countryName}
-            </p>
         </div>
     );
 };
--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 const ForecastCard = (props) => {
     const { forecast } = props;
@@ -8,12 +8,20 @@ const ForecastCard = (props) => {
         <div className='forecastCardWrapper flex justify-center gap-8 mt-8'>
             {forecast?.DailyForecasts?.slice(1)?.map((item, index) => {
                 return (
-                    <div key={index} className='forecastCard text-center w-28 h-24 rounded-md bg-green-700'>
-                        <p>{dayjs(item.Date).format('ddd')}</p>
-                        <img src={item.Day.Icon.toString().length == 1 ? `https://developer.accuweather.com/sites/default/files/0${item.Day.Icon}-s.png` : `https://developer.accuweather.com/sites/default/files/${item.Day.Icon}-s.png`}></img>
-                        <p>{item.Temperature.Minimum.Value}°C - {item.Temperature.Maximum.Value}°C</p>
+                    <div
+                        key={index}
+                        className='forecastCard text-center text-white w-32 h-32 py-3 rounded-md bg-[#1b1c48]'>
+                        <p>{dayjs(item.Date).format("ddd")}</p>
+                        <img
+                            className='mx-auto mt-2 mb-2'
+                            src={
+                                item.Day.Icon.toString().length == 1
+                                    ? `https://developer.accuweather.com/sites/default/files/0${item.Day.Icon}-s.png`
+                                    : `https://developer.accuweather.com/sites/default/files/${item.Day.Icon}-s.png`
+                            }></img>
+                        <p>{Math.floor(item.Temperature.Maximum.Value)}°C</p>
                     </div>
-                );  
+                );
             })}
         </div>
     );
